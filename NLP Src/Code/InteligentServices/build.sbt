d56d8aac95f514449d5f0e03bce3107ed51860e8
PrasannaMuppidi <pmyrd@mail.umkc.edu>
2016-07-05 20:19:58 -0500

    NLP

---
+++ NLP Src/Code/InteligentServices/build.sbt
@@ -0,0 +1,7 @@
+name := "InteligentServices"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "com.likethecolor" % "alchemy" % "1.2.0"
---
+++ NLP Src/Code/InteligentServices/src/main/java/uClassify.java
@@ -0,0 +1,53 @@
+/**
+ * Created by feichenshen on 3/1/16.
+ */
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class uClassify {
+
+    public static void main(String[] args) {
+
+        try {
+
+            URL url = new URL("http://uclassify.com/browse/uClassify/Text%20Language/ClassifyText?readkey=zBQAopgRZSXO&text=What+language+is+this+text%3f&output=json&version=1.01");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
