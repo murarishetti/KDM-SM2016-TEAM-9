dff7c41677c944d5e281bd2d68baa7dd16fa7579
anudeep1426 <anudeep.pandiri@gmail.com>
2016-07-08 02:17:54 -0500

    spark lda

--- src/src/main/java/DataCollection.java
+++ src/src/main/java/DataCollection.java
@@ -6,14 +6,12 @@ import java.net.URLConnection;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-/**
- * Created by chanti on 24-Jun-16.
- */
+
 public class DataCollection {
     public static String main() throws IOException {
         String fileName = "gTrends.txt";
         String url2 = "https://www.google.com/trends/api/stories/latest?cat=m&fi=15&fs=15&geo=US&ri=300&rs=15&tz=300";
-        URL url = new URL(url2);
+        URL url = new URL(url2);8
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         urlConnection.setRequestMethod("GET");
         urlConnection.setDoInput(true);
